edae9cfa9e81f8e560440c5ad7edf7bbd00ac3c5
Prasathsing <prasathprakash17@gmail.com>
2022-03-25 17:52:52 +0530

    Mobile Option

--- src/step/Sample.java
+++ src/step/Sample.java
@@ -10,11 +10,15 @@ System.out.println(age);
 	public void email(String email) {
 System.out.println(email);
 	}
+	public void phoneNumber(long mob) {
+System.out.println(mob);
+	}
 public static void main(String[] args) {
 	Sample s=new Sample();
 	s.name("Prasathsing");
 	s.age(24);
 	s.email("prasathprakash17@gmail.com");
+	s.phoneNumber(9047754505l);
 	
 }
 }
