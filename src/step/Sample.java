51a7fd45af534962a4b65b48ab1f36f223aa80ff
Prasathsing <prasathprakash17@gmail.com>
2022-03-25 17:40:47 +0530

    Folder structure of project

---
+++ src/step/Sample.java
@@ -0,0 +1,20 @@
+package step;
+
+public class Sample {
+	public void name(String name) {
+System.out.println(name);
+	}
+	public void age(int age) {
+System.out.println(age);
+	}
+	public void email(String email) {
+System.out.println(email);
+	}
+public static void main(String[] args) {
+	Sample s=new Sample();
+	s.name("Prasathsing");
+	s.age(24);
+	s.email("prasathprakash17@gmail.com");
+	
+}
+}
